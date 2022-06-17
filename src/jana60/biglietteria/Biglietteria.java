d8634bfb7b70c9bc21c495bde5e1d98254e4abd0
Pier95F <forcinapierpaolo@gmail.com>
2022-06-17 15:51:04 +0200

    terminata classe biglietto

--- src/jana60/biglietteria/Biglietteria.java
+++ src/jana60/biglietteria/Biglietteria.java
@@ -3,7 +3,7 @@ package jana60.biglietteria;
 public class Biglietteria {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/jana60/biglietteria/Biglietto.java
+++ src/jana60/biglietteria/Biglietto.java
@@ -1,5 +1,33 @@
 package jana60.biglietteria;
 
+import java.text.DecimalFormat;
+
 public class Biglietto {
 
+		// Definisco gli attributi della classe
+		private int eta, km;
+		private double prezzoKm = 0.21, scontoUnder = 0.2, scontoOver = 0.4;
+		
+		DecimalFormat df = new DecimalFormat("#.## €");
+
+		// Costruttori
+		public Biglietto(int eta, int km) {
+			super();
+			this.eta = eta;
+			this.km = km;
+		}
+		
+		// Imposto i getter
+		public int getKm(int km) {
+			this.km = km;
+			return km;
+		}
+		
+		public int getEta (int eta) {
+			this.eta = eta;
+			return eta;
+		}
+		
+		// Metodi
+		
 }
